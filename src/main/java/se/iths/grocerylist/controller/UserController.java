741e0d1242c47b7183193af024ab7473eb70a47f
Sofie Holden <Sofie.Holden@cybercom.com>
2021-11-30 16:26:48 +0100

    Added CRUD to UserService & UserController

--- src/main/java/se/iths/grocerylist/controller/UserController.java
+++ src/main/java/se/iths/grocerylist/controller/UserController.java
@@ -1,7 +1,64 @@
 package se.iths.grocerylist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.boot.jackson.JsonObjectSerializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.grocerylist.entity.UserEntity;
+import se.iths.grocerylist.service.UserService;
+
+import javax.annotation.security.DenyAll;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
+        UserEntity createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<UserEntity>> findUserById(@PathVariable Long id){
+        Optional<UserEntity> foundUser = userService.findUserById(id);
+        return new ResponseEntity<>(foundUser, HttpStatus.FOUND);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<UserEntity>> findAllUsers(){
+        Iterable<UserEntity> allUsers = userService.findAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.FOUND);
+    }
+
+    @PutMapping()
+    public ResponseEntity<UserEntity>updateUser(@RequestBody UserEntity user){
+       UserEntity updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<Optional<UserEntity>> updateUserEmail(@PathVariable Long id, @RequestBody UserEntity user){
+        Optional<UserEntity> updatedUser = userService.updateUserEmail(id, user.getMail());
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+
 }
