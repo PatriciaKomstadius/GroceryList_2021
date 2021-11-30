f8c0233b4ed92fe64d674cbf54f1036348d1655a
Sofie Holden <Sofie.Holden@cybercom.com>
2021-11-30 14:05:23 +0100

    Fixed bugs so now the project starts

--- src/main/java/se/iths/grocerylist/repository/UserRepository.java
+++ src/main/java/se/iths/grocerylist/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package se.iths.grocerylist.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.iths.grocerylist.entity.UserEntity;
 
-
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
 
