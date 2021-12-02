76a139804392f999f982cf599f055e02f518272c
anden <andenk@hotmail.se>
2021-12-02 10:25:33 +0100

    CRUD User

--- src/main/java/se/iths/grocerylist/service/UserService.java
+++ src/main/java/se/iths/grocerylist/service/UserService.java
@@ -1,7 +1,10 @@
 package se.iths.grocerylist.service;
 
 import org.springframework.stereotype.Service;
+import se.iths.grocerylist.entity.UserEntity;
 import se.iths.grocerylist.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,5 +14,37 @@ public class UserService {
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+    public UserEntity createUser (UserEntity user){
+        return userRepository.save(user);
+    }
+
+    public Optional<UserEntity> findUserById (Long id){
+        return userRepository.findById(id);
+    }
+
+    public Iterable<UserEntity>findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public UserEntity updateUser(UserEntity user){
+        userRepository.save(user);
+
+        return user;
+    }
+
+    public Optional<UserEntity> updateUserEmail(Long id, String email){
+        Optional<UserEntity> foundUser = userRepository.findById(id);
+        foundUser.get().setMail(email);
+        userRepository.save(foundUser.get());
+        return foundUser;
+
+    }
+
+    public void deleteUser(Long id){
+        Optional<UserEntity> foundUser = userRepository.findById(id);
+        userRepository.deleteById(foundUser.get().getId());
+    }
+
 
 }
