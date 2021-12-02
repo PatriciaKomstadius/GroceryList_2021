1eab50ee9c0735326158707527bd7b4b7a194456
anden <andenk@hotmail.se>
2021-12-02 10:53:54 +0100

    department and comapanys CRUD

--- src/main/java/se/iths/grocerylist/service/CompanyService.java
+++ src/main/java/se/iths/grocerylist/service/CompanyService.java
@@ -1,7 +1,10 @@
 package se.iths.grocerylist.service;
 
 import org.springframework.stereotype.Service;
+import se.iths.grocerylist.entity.CompanyEntity;
 import se.iths.grocerylist.repository.CompanyRepository;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -11,4 +14,38 @@ public class CompanyService {
     public CompanyService(CompanyRepository companyRepository){
         this.companyRepository = companyRepository;
     }
+
+
+    public CompanyEntity createCompany(CompanyEntity company) {
+        return companyRepository.save(company);
+    }
+
+    public Optional<CompanyEntity> findCompanyById (Long id){
+        return companyRepository.findById(id);
+    }
+
+    public Iterable<CompanyEntity>findAllCompanys(){
+        return companyRepository.findAll();
+    }
+
+    public CompanyEntity updateCompany(CompanyEntity company){
+        companyRepository.save(company);
+        return company;
+    }
+
+    public Optional<CompanyEntity> updateCompanyName(Long id, String companyName){
+        Optional<CompanyEntity> foundCompany = companyRepository.findById(id);
+        foundCompany.get().setCompanyName(companyName);
+        companyRepository.save(foundCompany.get());
+        return foundCompany;
+
+    }
+
+    public void deleteCompany(Long id){
+        Optional<CompanyEntity> foundCompany = companyRepository.findById(id);
+        companyRepository.deleteById(foundCompany.get().getId());
+    }
+
+
+
 }
