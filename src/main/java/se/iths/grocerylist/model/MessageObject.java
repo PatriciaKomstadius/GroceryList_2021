949ad8f90691e933269469753a7f9309a42e3efb
Sofie Holden <Sofie.Holden@cybercom.com>
2021-12-14 14:59:39 +0100

    Added JMS Sender & Receiver

---
+++ src/main/java/se/iths/grocerylist/model/MessageObject.java
@@ -0,0 +1,56 @@
+package se.iths.grocerylist.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class MessageObject implements Serializable {
+
+    private UUID id;
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime localDateTime;
+
+    public MessageObject(UUID id, String message, LocalDateTime localDateTime) {
+        this.id = id;
+        this.message = message;
+        this.localDateTime = localDateTime;
+    }
+
+    public MessageObject(){}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageObject{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", localDateTime=" + localDateTime +
+                '}';
+    }
+}
