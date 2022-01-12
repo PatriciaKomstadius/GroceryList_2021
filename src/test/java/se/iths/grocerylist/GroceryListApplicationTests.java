744f3b3499e18b191e0f0690150364d2bf97ccf5
anden <andenk@hotmail.se>
2022-01-12 14:10:32 +0100

    four test

--- src/test/java/se/iths/grocerylist/GroceryListApplicationTests.java
+++ src/test/java/se/iths/grocerylist/GroceryListApplicationTests.java
@@ -1,26 +1,19 @@
 package se.iths.grocerylist;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import se.iths.grocerylist.entity.*;
-import se.iths.grocerylist.repository.GroceryListRepository;
-import se.iths.grocerylist.repository.RoleRepository;
-import se.iths.grocerylist.repository.UserRepository;
-import se.iths.grocerylist.view.MvcConfig;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,11 +29,6 @@ class GroceryListApplicationTests {
     @Autowired
     TestRestTemplate testClient;
 
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private UserRepository userRepository;
 
     @BeforeEach
     void beforeTestLoadDatabaseWithUsers(){
@@ -65,18 +53,49 @@ class GroceryListApplicationTests {
     void oneShouldReturnOneUsername() {
         String url = "http://localhost:" + port + "/users/{id}";
 
-
-
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth("admin", "123");
 
         long id = 4;
         var result = testClient.exchange(url, HttpMethod.GET, new HttpEntity<>(headers),UserEntity.class,id);
 
+        assertThat(result.getBody().getUsername().equals("admin"));
 
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.FOUND);
 
+    }
+
+
+    @Test
+    void deleteUserShouldReturnNoContent() {
+        String url = "http://localhost:" + port + "/users/{id}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("admin", "123");
+
+        long id = 4;
+        var result = testClient.exchange(url, HttpMethod.DELETE, new HttpEntity<>(headers),UserEntity.class,id);
+
+
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 
     }
 
+    @Test
+    void createdGrocerylistShoudeReturnStatusCreated(){
+
+        String url = "http://localhost:" + port + "/grocerylists";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("admin", "123");
+        headers.add("Content-Type", "application/json");
+
+
+        GroceryListEntity groceryListEntity = new GroceryListEntity("Julmat");
+       var result = testClient.exchange(url, HttpMethod.POST, new HttpEntity<Object>(groceryListEntity, headers), GroceryListEntity.class);
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+    }
 }
